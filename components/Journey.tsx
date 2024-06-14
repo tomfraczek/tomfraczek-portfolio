caae26925974549b48695653bbc453f79851bfd1
unknown <tomfraczekdev@gmail.com>
2024-06-14 20:12:32 +0200

    feat: add journey and questions components

---
+++ components/Journey.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+export const Journey = () => {
+  return (
+    <div className="py-10 w-2/3">
+      <h1 className="text-center font-bold text-4xl fjalla">JOURNEY</h1>
+      <p className="my-6 nunito">
+        I am a dedicated Frontend Developer with a passion for creating web and
+        mobile applications using React and React Native technologies. My
+        journey in development began in London in 2017, where I started as a
+        freelancer, crafting my first commercial websites.
+      </p>
+      <p className="my-6 nunito">
+        The next step in my career was joining a London-based company. Here, I
+        had the opportunity to use the React library in a real-world
+        environment, creating interfaces and collaborating with a talented team.
+        This experience was instrumental in honing my skills and understanding
+        the practical applications of React.
+      </p>
+      <p className="my-6 nunito">
+        The highlight of my career so far has been my time at a software house,
+        where I could focus entirely on advancing my hard programming skills as
+        a React Developer. This role allowed me to grow technically and solidify
+        my expertise in building sophisticated and efficient applications.
+      </p>
+
+      <p className="my-6 nunito">
+        In addition to my frontend expertise, I have also started developing my
+        skills in backend technologies such as Node.js, Express, and MongoDB. My
+        goal is to become a complete developer, capable of handling both
+        frontend and backend development seamlessly.
+      </p>
+      <p className="my-6 nunito">
+        At work I am known for my positive attitude, goal-oriented personality,
+        and strong motivation to continually enhance my skills.
+      </p>
+      <p className="my-6 nunito">
+        Programming is not just my profession but also a source of joy and
+        fulfillment for me. I am always eager to take on new challenges and push
+        the boundaries of what can be achieved through code.
+      </p>
+    </div>
+  );
+};
---
+++ components/Questions.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Questions = () => {
+  return (
+    <div className="py-10 w-2/3">
+      <h1 className="text-center font-bold text-4xl fjalla">
+        What is a Frontend Developer
+      </h1>
+      <p className="my-6 nunito">
+        A Frontend Developer specializes in building the visual and interactive
+        parts of websites and web applications that users interact with
+        directly. This involves creating interfaces using HTML, CSS, and
+        JavaScript, and ensuring that the design is responsive, user-friendly,
+        and performs well across different devices and browsers. Frontend
+        developers focus on implementing design mockups, adding interactivity,
+        optimizing performance, and enhancing the overall user experience.
+      </p>
+      <h1 className="text-center font-bold text-4xl fjalla">Why backend?</h1>
+      <p className="my-6 nunito">
+        Although my primary focus has been on frontend development, I am
+        learning backend technologies like Node.js, Express, and MongoDB to
+        become a more versatile developer. Understanding both frontend and
+        backend allows me to work on entire projects independently, improve
+        collaboration with backend developers, and troubleshoot issues more
+        effectively. Additionally, having a broad skill set opens up more career
+        opportunities and enables me to contribute to various aspects of a
+        project, making me a more valuable asset in the tech industry.
+      </p>
+    </div>
+  );
+};
