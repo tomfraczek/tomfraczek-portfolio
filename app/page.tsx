b7f3b03147334f23800af64835f5320f56f718f3
unknown <tomfraczekdev@gmail.com>
2024-06-14 20:10:59 +0200

    feat: add header and main navigation to the layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 import Image from "next/image";
+import { Hero } from "../components/Hero";
+import { StackBanner } from "../components/StackBanner";
+import { Journey } from "@/components/Journey";
+import { Questions } from "@/components/Questions";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center px-24  overflow-hidden">
+      <Hero />
+      <StackBanner />
+      <Journey />
+      <Questions />
+    </main>
   );
 }
